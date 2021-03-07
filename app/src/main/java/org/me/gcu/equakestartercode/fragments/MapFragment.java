ceed45b448e496853c8267538e89cef7af2d8417
hng201 <hng201@caledonian.ac.uk>
2021-03-07 18:27:29 +0000

    Initial commit - Loading of Google Maps implemented

---
+++ app/src/main/java/org/me/gcu/equakestartercode/fragments/MapFragment.java
@@ -0,0 +1,51 @@
+package org.me.gcu.equakestartercode.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.me.gcu.equakestartercode.R;
+
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+    private GoogleMap gmap;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) this.getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync( this);
+        return view;
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        gmap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        gmap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        gmap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
