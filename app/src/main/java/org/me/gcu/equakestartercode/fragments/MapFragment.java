ad3073240f30c31a1515e8cc3a5867ca2fb97e80
hng201 <hng201@caledonian.ac.uk>
2021-03-11 19:55:15 +0000

    Implemented GoogleMaps markers for each earthquakes location and FragmentResultListener to get the earthquakeList from HomeFragment

--- app/src/main/java/org/me/gcu/equakestartercode/fragments/MapFragment.java
+++ app/src/main/java/org/me/gcu/equakestartercode/fragments/MapFragment.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentResultListener;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,9 +16,13 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.me.gcu.equakestartercode.R;
+import org.me.gcu.equakestartercode.models.Earthquake;
 
-public class MapFragment extends Fragment implements OnMapReadyCallback {
+import java.util.ArrayList;
+
+public class MapFragment extends Fragment implements OnMapReadyCallback{
     private GoogleMap gmap;
+    private ArrayList<Earthquake> earthquakeList = new ArrayList<>();;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -30,22 +35,47 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     }
 
     /**
+     * Sets a FragmentResultListener which will trigger when a result
+     * with the result key 'elist' is set.
+     * The earthquakeList is obtained here from the HomeFragment.
+     * @param savedInstanceState
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getParentFragmentManager().setFragmentResultListener("elist", this, new FragmentResultListener() {
+            @Override
+            public void onFragmentResult(String requestKey, Bundle bundle) {
+                // Get the value of the key 'earthquakes from the bundle
+                earthquakeList = (ArrayList<Earthquake>) bundle.getSerializable("earthquakes");
+            }
+        });
+    }
+
+    /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
+     * The location of each earthquake is also marked in the map.
+     * @param googleMap
      */
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
         gmap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        gmap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        gmap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        for (Earthquake earthquake: earthquakeList){
+            // Create a new coordinates based on the earthquakes geo latitude and geo longitude
+            LatLng latLng = new LatLng(earthquake.getGeoLat(), earthquake.getGeoLong());
+            // Add the location of the earthquake as a marker on GoogleMaps based on its coordinates
+            // Set the title to the earthquake's location name
+            gmap.addMarker(new MarkerOptions().position(latLng).title(earthquake.getLocation()));
+        }
+        // Coordinates for London
+        LatLng london = new LatLng(51.509865, -0.118092);
+        // Moves the camera to the location of London
+        // This is to just focus the camera on the UK
+        gmap.moveCamera(CameraUpdateFactory.newLatLng(london));
+        // Displays the Zoom in and out controls on the Map UI
+        gmap.getUiSettings().setZoomControlsEnabled(true);
     }
+
 }
