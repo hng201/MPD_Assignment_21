f766b6e578dde1aeec13479fb7b97d7a0c2bccba
hng201 <hng201@caledonian.ac.uk>
2021-03-22 23:02:15 +0000

    Implemented onDateSet and onDismiss for DatePickerDialog and setDate method for updating EditText value with the date selected

--- app/src/main/java/org/me/gcu/equakestartercode/fragments/SearchFragment.java
+++ app/src/main/java/org/me/gcu/equakestartercode/fragments/SearchFragment.java
@@ -1,18 +1,25 @@
 package org.me.gcu.equakestartercode.fragments;
 
 import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
 import org.me.gcu.equakestartercode.R;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Shirley Ng S1626790
@@ -24,6 +31,12 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
     private Button btnSearchByDate;
     private Button btnSearchByDateRange;
     private DatePickerDialog dpd;
+    private Calendar cal;
+    private Date date;
+    private Date startDate;
+    private Date endDate;
+    private Boolean dateSelected;
+
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
@@ -44,22 +57,54 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
 
         dpd = new DatePickerDialog(getContext(), 0);
 
+        dateSelected = false;
 
         return view;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void setDate(EditText et){
+        dpd.show();
+        dpd.setOnDateSetListener( new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                cal = Calendar.getInstance();
+                cal.set(year, month, dayOfMonth);
+                dateSelected = true;
+            }
+        });
+
+        dpd.setOnDismissListener( new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialog) {
+                Date temp = cal.getTime();
+                if (et == etDate) {
+                    date = temp;
+                } else if (etEndDate == etStartDate) {
+                    startDate = temp;
+                } else {
+                    endDate = temp;
+                }
+                DateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+                String strDate = sdf.format(temp);
+                et.setText(strDate);
+            }
+        });
+
+    }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public void onClick(View v) {
         if (v == etDate){
-            dpd.show();
+            setDate(etDate);
+
         }
         else if (v == etStartDate){
-            dpd.show();
+            setDate(etStartDate);
         }
         else if (v == etEndDate) {
-            dpd.show();
+            setDate(etEndDate);
         }
     }
 
