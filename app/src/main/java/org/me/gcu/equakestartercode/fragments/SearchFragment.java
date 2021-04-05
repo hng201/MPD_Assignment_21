d2cb43a808f1a895802582eba10f77fdca8eadf1
hng201 <hng201@caledonian.ac.uk>
2021-04-05 21:48:32 +0100

    Implemented view switcher

--- app/src/main/java/org/me/gcu/equakestartercode/fragments/SearchFragment.java
+++ app/src/main/java/org/me/gcu/equakestartercode/fragments/SearchFragment.java
@@ -10,6 +10,9 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.ViewSwitcher;
 
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
@@ -18,6 +21,7 @@ import org.me.gcu.equakestartercode.R;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -30,13 +34,14 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
     private EditText etEndDate;
     private Button btnSearchByDate;
     private Button btnSearchByDateRange;
+    private RadioButton rbtnDate;
+    private RadioButton rbtnDateRange;
     private DatePickerDialog dpd;
     private Calendar cal;
-    private Date date;
-    private Date startDate;
-    private Date endDate;
-    private Boolean dateSelected;
-
+    private ArrayList<String> dates = new ArrayList<>();
+    private ViewSwitcher viewSwitcher;
+    private boolean byDate;
+    private RadioGroup radioGroup;
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
@@ -48,6 +53,8 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
         etEndDate = view.findViewById(R.id.etEndDate);
         btnSearchByDate = view.findViewById(R.id.btnSearchByDate);
         btnSearchByDateRange = view.findViewById(R.id.btnSearchByDateRange);
+        rbtnDate = view.findViewById(R.id.rbtnDate);
+        rbtnDateRange = view.findViewById(R.id.rbtnDateRange);
 
         etDate.setOnClickListener(this);
         etStartDate.setOnClickListener(this);
@@ -55,9 +62,17 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
         btnSearchByDate.setOnClickListener(this);
         btnSearchByDateRange.setOnClickListener(this);
 
-        dpd = new DatePickerDialog(getContext(), 0);
+        radioGroup = view.findViewById(R.id.rgSearch);
 
-        dateSelected = false;
+        rbtnDate.toggle();
+        rbtnDate.setOnClickListener(this);
+        rbtnDateRange.setOnClickListener(this);
+
+        radioGroup.setEnabled(true);
+        viewSwitcher = view.findViewById(R.id.viewSwitcher);
+        byDate = true;
+
+        dpd = new DatePickerDialog(getContext(), 0);
 
         return view;
     }
@@ -70,7 +85,6 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
                 cal = Calendar.getInstance();
                 cal.set(year, month, dayOfMonth);
-                dateSelected = true;
             }
         });
 
@@ -78,13 +92,6 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
             @Override
             public void onDismiss(DialogInterface dialog) {
                 Date temp = cal.getTime();
-                if (et == etDate) {
-                    date = temp;
-                } else if (etEndDate == etStartDate) {
-                    startDate = temp;
-                } else {
-                    endDate = temp;
-                }
                 DateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
                 String strDate = sdf.format(temp);
                 et.setText(strDate);
@@ -96,15 +103,17 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public void onClick(View v) {
-        if (v == etDate){
-            setDate(etDate);
-
+        if (rbtnDate.isChecked()){
+            if (!byDate){
+                viewSwitcher.showNext();
+                byDate = true;
+            }
         }
-        else if (v == etStartDate){
-            setDate(etStartDate);
-        }
-        else if (v == etEndDate) {
-            setDate(etEndDate);
+        else if (rbtnDateRange.isChecked()){
+            if (byDate){
+                viewSwitcher.showNext();
+                byDate = false;
+            }
         }
     }
 
