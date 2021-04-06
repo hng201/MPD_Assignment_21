994deb1f122b98770cbc3fc59fa2267bc062ee43
hng201 <hng201@caledonian.ac.uk>
2021-04-06 19:57:59 +0100

    Implemented shallowest and deepest earthquake and text view for search criteria

--- app/src/main/java/org/me/gcu/equakestartercode/fragments/SearchResultFragment.java
+++ app/src/main/java/org/me/gcu/equakestartercode/fragments/SearchResultFragment.java
@@ -22,11 +22,16 @@
   * Shirley Ng S1626790
   */
 public class SearchResultFragment extends Fragment {
+    private TextView tvSearchCriteria;
     private TextView tvLargeMag;
-    private TextView tvNull;
+    private TextView tvEarthquakeNo;
+    private TextView tvShallowest;
+    private TextView tvDeepest;
     private ArrayList<Earthquake> filteredList = new ArrayList<>();
     private ArrayList<String> dates = new ArrayList<>();
-    private Earthquake largestMag;
+    private Earthquake largestMagEq;
+    private Earthquake shallowestEq;
+    private Earthquake deepestEq;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,8 +43,11 @@ public class SearchResultFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_results, container, false);
 
+        tvSearchCriteria = view.findViewById(R.id.tvSearchCriteria);
         tvLargeMag = view.findViewById(R.id.tvLargeMag);
-        tvNull = view.findViewById(R.id.tvNull);
+        tvEarthquakeNo = view.findViewById(R.id.tvEarthquakeNo);
+        tvShallowest = view.findViewById(R.id.tvShallowest);
+        tvDeepest = view.findViewById(R.id.tvDeepest);
 
         new GetFilteredEarthquakesTask().execute(dates.get(0));
 
@@ -52,7 +60,9 @@ public class SearchResultFragment extends Fragment {
             Context context = getContext();
             AppDatabase db = Room.databaseBuilder(context, AppDatabase.class, "db-earthquake").build();
             Log.e("Param", params[0]);
-            largestMag = db.earthquakeDao().getLargestMagnitudeEarthquake();
+            largestMagEq = db.earthquakeDao().getLargestMagnitudeEarthquake(params[0]);
+            shallowestEq = db.earthquakeDao().getShallowestEarthquake(params[0]);
+            deepestEq = db.earthquakeDao().getDeepestEarthquake(params[0]);
             return (ArrayList<Earthquake>) db.earthquakeDao().getEarthquakesByDate(params[0]);
         }
 
@@ -63,19 +73,24 @@ public class SearchResultFragment extends Fragment {
             Log.e("List", result.toString());
             filteredList = result;
             Log.e("Filtered List", filteredList.toString());
+            tvSearchCriteria.setText(dates.get(0));
             if (filteredList.isEmpty()){
-                tvNull.setText("No Earthquakes Found");
+                tvEarthquakeNo.setText("No Earthquakes Found");
             }
             else {
                 getEarthquakeSummary(filteredList);
-                tvNull.setText("Number of Earthquakes Found: " + filteredList.size());
+                tvEarthquakeNo.setText("Number of Earthquakes Found: " + filteredList.size());
             }
         }
     }
 
     private void getEarthquakeSummary(ArrayList<Earthquake> earthquakeList) {
-        String largestMagnitude = largestMag.getLocation() + " M" + largestMag.getMagnitude();
+        String largestMagnitude = largestMagEq.getLocation() + " M" + largestMagEq.getMagnitude();
         tvLargeMag.setText(largestMagnitude);
+        String shallowest = shallowestEq.getLocation() + " Depth: " + shallowestEq.getDepth();
+        tvShallowest.setText(shallowest);
+        String deepest = deepestEq.getLocation() + " Depth: " + deepestEq.getDepth();
+        tvDeepest.setText(deepest);
     }
 
     public void setDates(ArrayList<String> temp){
