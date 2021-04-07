c6eb22a705aec57f2bd2e5a7dd5fd9f2cec27a5f
hng201 <hng201@caledonian.ac.uk>
2021-04-07 22:02:02 +0100

    Implemented colour coded markers

--- app/src/main/java/org/me/gcu/equakestartercode/fragments/MapFragment.java
+++ app/src/main/java/org/me/gcu/equakestartercode/fragments/MapFragment.java
@@ -2,18 +2,19 @@ package org.me.gcu.equakestartercode.fragments;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentResultListener;
 import androidx.room.Room;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -28,7 +29,7 @@ import java.util.ArrayList;
  */
 public class MapFragment extends Fragment implements OnMapReadyCallback{
     private GoogleMap gmap;
-    private ArrayList<Earthquake> earthquakeList = new ArrayList<>();;
+    private ArrayList<Earthquake> earthquakeList = new ArrayList<>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,6 +39,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_map, container, false);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) this.getChildFragmentManager()
                 .findFragmentById(R.id.map);
@@ -57,6 +59,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback{
         {
             super.onPostExecute(result);
             earthquakeList = result;
+            Log.e("MapList", earthquakeList.toString());
         }
     }
 
@@ -64,7 +67,11 @@ public class MapFragment extends Fragment implements OnMapReadyCallback{
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * The location of each earthquake is also marked in the map.
+     * The location of each earthquake is also marked in the map
+     * and colour coded based on magnitude strength.
+     * NOTE: Colour coding here does not match the earthquake
+     * colour coding outside of the MapView due to the limitted
+     * options of colours for the markers
      * @param googleMap
      */
     @Override
@@ -75,8 +82,47 @@ public class MapFragment extends Fragment implements OnMapReadyCallback{
             // Create a new coordinates based on the earthquakes geo latitude and geo longitude
             LatLng latLng = new LatLng(earthquake.getGeoLat(), earthquake.getGeoLong());
             // Add the location of the earthquake as a marker on GoogleMaps based on its coordinates
-            // Set the title to the earthquake's location name
-            gmap.addMarker(new MarkerOptions().position(latLng).title(earthquake.getLocation()));
+            if (earthquake.getMagnitude() <= 1){
+                // Set the title to the earthquake's location name
+                gmap.addMarker(new MarkerOptions().position(latLng).title(earthquake.getLocation())).setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+            }
+            else if (earthquake.getMagnitude() <= 2) {
+                // Set the title to the earthquake's location name
+                gmap.addMarker(new MarkerOptions().position(latLng).title(earthquake.getLocation())).setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN));
+            }
+            else if (earthquake.getMagnitude() <= 3) {
+                // Set the title to the earthquake's location name
+                gmap.addMarker(new MarkerOptions().position(latLng).title(earthquake.getLocation())).setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+            }
+            else if (earthquake.getMagnitude() <= 4) {
+                // Set the title to the earthquake's location name
+                gmap.addMarker(new MarkerOptions().position(latLng).title(earthquake.getLocation())).setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+            }
+            else if (earthquake.getMagnitude() <= 5) {
+                // Set the title to the earthquake's location name
+                gmap.addMarker(new MarkerOptions().position(latLng).title(earthquake.getLocation())).setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET));
+            }
+            else if (earthquake.getMagnitude() <= 6) {
+                // Set the title to the earthquake's location name
+                gmap.addMarker(new MarkerOptions().position(latLng).title(earthquake.getLocation())).setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
+            }
+            else if (earthquake.getMagnitude() <= 7) {
+                // Set the title to the earthquake's location name
+                gmap.addMarker(new MarkerOptions().position(latLng).title(earthquake.getLocation())).setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE));
+            }
+            else if (earthquake.getMagnitude() <= 8) {
+                // Set the title to the earthquake's location name
+                gmap.addMarker(new MarkerOptions().position(latLng).title(earthquake.getLocation())).setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW));
+            }
+            else if (earthquake.getMagnitude() <= 9) {
+                // Set the title to the earthquake's location name
+                gmap.addMarker(new MarkerOptions().position(latLng).title(earthquake.getLocation())).setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE));
+            }
+            else {
+                // Set the title to the earthquake's location name
+                gmap.addMarker(new MarkerOptions().position(latLng).title(earthquake.getLocation())).setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+            }
+
         }
         // Coordinates for London
         LatLng london = new LatLng(51.509865, -0.118092);
