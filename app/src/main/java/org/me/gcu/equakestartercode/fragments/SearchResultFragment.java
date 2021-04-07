382c79ec72d385502b5defac3fff5ca0cd17cafd
hng201 <hng201@caledonian.ac.uk>
2021-04-07 14:00:16 +0100

    Implement search for by date range and change search criteria button

--- app/src/main/java/org/me/gcu/equakestartercode/fragments/SearchResultFragment.java
+++ app/src/main/java/org/me/gcu/equakestartercode/fragments/SearchResultFragment.java
@@ -7,21 +7,25 @@
  import android.view.LayoutInflater;
  import android.view.View;
  import android.view.ViewGroup;
+ import android.widget.Button;
  import android.widget.TextView;
 
  import androidx.fragment.app.Fragment;
+ import androidx.fragment.app.FragmentTransaction;
  import androidx.room.Room;
 
  import org.me.gcu.equakestartercode.R;
  import org.me.gcu.equakestartercode.database.AppDatabase;
  import org.me.gcu.equakestartercode.models.Earthquake;
 
+ import java.util.Date;
+ import java.text.SimpleDateFormat;
  import java.util.ArrayList;
 
  /**
   * Shirley Ng S1626790
   */
-public class SearchResultFragment extends Fragment {
+public class SearchResultFragment extends Fragment implements View.OnClickListener {
     private TextView tvSearchCriteria;
     private TextView tvNorth;
     private TextView tvSouth;
@@ -31,7 +35,6 @@ public class SearchResultFragment extends Fragment {
     private TextView tvEarthquakeNo;
     private TextView tvShallowest;
     private TextView tvDeepest;
-    private ArrayList<Earthquake> filteredList = new ArrayList<>();
     private ArrayList<String> dates = new ArrayList<>();
     private Earthquake northEq;
     private Earthquake southEq;
@@ -40,12 +43,9 @@ public class SearchResultFragment extends Fragment {
     private Earthquake largestMagEq;
     private Earthquake shallowestEq;
     private Earthquake deepestEq;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Log.e("Search Criteria", dates.get(0));
-    }
+    private Button btnChangeSearch;
+    private String start;
+    private  String end;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -60,26 +60,65 @@ public class SearchResultFragment extends Fragment {
         tvEarthquakeNo = view.findViewById(R.id.tvEarthquakeNo);
         tvShallowest = view.findViewById(R.id.tvShallowest);
         tvDeepest = view.findViewById(R.id.tvDeepest);
+        btnChangeSearch = view.findViewById(R.id.btnChangeSearch);
+        btnChangeSearch.setOnClickListener(this);
 
-        new GetFilteredEarthquakesTask().execute(dates.get(0));
+        new GetFilteredEarthquakesTask().execute(dates);
 
         return view;
     }
 
-    private class GetFilteredEarthquakesTask extends AsyncTask<String, Void, ArrayList<Earthquake>> {
+     @Override
+     public void onClick(View v) {
+         if (v == btnChangeSearch){
+             SearchFragment searchFragment = new SearchFragment();
+             FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+             // Replace current fragment with new fragment
+             transaction.replace(R.id.frameLayout, searchFragment);
+             transaction.commit();
+         }
+     }
+
+     private class GetFilteredEarthquakesTask extends AsyncTask<ArrayList<String>, Void, ArrayList<Earthquake>> {
         @Override
-        protected ArrayList<Earthquake> doInBackground(String... params) {
+        protected ArrayList<Earthquake> doInBackground(ArrayList<String>... params) {
             Context context = getContext();
             AppDatabase db = Room.databaseBuilder(context, AppDatabase.class, "db-earthquake").build();
-            Log.e("Param", params[0]);
-            northEq = db.earthquakeDao().getNorthEarthquake(params[0]);
-            southEq = db.earthquakeDao().getSouthEarthquake(params[0]);
-            eastEq = db.earthquakeDao().getEastEarthquake(params[0]);
-            westEq = db.earthquakeDao().getWestEarthquake(params[0]);
-            largestMagEq = db.earthquakeDao().getLargestMagnitudeEarthquake(params[0]);
-            shallowestEq = db.earthquakeDao().getShallowestEarthquake(params[0]);
-            deepestEq = db.earthquakeDao().getDeepestEarthquake(params[0]);
-            return (ArrayList<Earthquake>) db.earthquakeDao().getEarthquakesByDate(params[0]);
+            ArrayList<String> searchCriteria = params[0];
+            if (searchCriteria.size() > 1){
+                Log.e("Search Criteria", searchCriteria.get(0) + " to " + searchCriteria.get(1));
+                try {
+                    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+                    SimpleDateFormat sdfSearchFormat = new SimpleDateFormat("yyyy-MM-dd");
+                    Date date1 = sdf.parse(searchCriteria.get(0));
+                    Date date2 = sdf.parse(searchCriteria.get(1));
+                    start = sdfSearchFormat.format(date1);
+                    end = sdfSearchFormat.format(date2);
+                }
+                catch (Exception e){
+                    Log.e("Exception", e.toString());
+                }
+
+                northEq = db.earthquakeDao().getNorthEarthquakeByDateRange(start, end);
+                southEq = db.earthquakeDao().getSouthEarthquakeByDateRange(start, end);
+                eastEq = db.earthquakeDao().getEastEarthquakeByDateRange(start, end);
+                westEq = db.earthquakeDao().getWestEarthquakeByDateRange(start, end);
+                largestMagEq = db.earthquakeDao().getLargestMagnitudeEarthquakeByDateRange(start, end);
+                shallowestEq = db.earthquakeDao().getShallowestEarthquakeByDateRange(start, end);
+                deepestEq = db.earthquakeDao().getDeepestEarthquakeByDateRange(start, end);
+                return (ArrayList<Earthquake>) db.earthquakeDao().getEarthquakesByDateRange(start, end);
+            }
+            else {
+                Log.e("Search Criteria", searchCriteria.get(0));
+                northEq = db.earthquakeDao().getNorthEarthquake(searchCriteria.get(0));
+                southEq = db.earthquakeDao().getSouthEarthquake(searchCriteria.get(0));
+                eastEq = db.earthquakeDao().getEastEarthquake(searchCriteria.get(0));
+                westEq = db.earthquakeDao().getWestEarthquake(searchCriteria.get(0));
+                largestMagEq = db.earthquakeDao().getLargestMagnitudeEarthquake(searchCriteria.get(0));
+                shallowestEq = db.earthquakeDao().getShallowestEarthquake(searchCriteria.get(0));
+                deepestEq = db.earthquakeDao().getDeepestEarthquake(searchCriteria.get(0));
+                return (ArrayList<Earthquake>) db.earthquakeDao().getEarthquakesByDate(searchCriteria.get(0));
+            }
         }
 
         @Override
@@ -87,20 +126,24 @@ public class SearchResultFragment extends Fragment {
         {
             super.onPostExecute(result);
             Log.e("List", result.toString());
-            filteredList = result;
-            Log.e("Filtered List", filteredList.toString());
-            tvSearchCriteria.setText(dates.get(0));
-            if (filteredList.isEmpty()){
+            Log.e("Filtered List", result.toString());
+            if (dates.size() > 1){
+                tvSearchCriteria.setText(dates.get(0) + " to " + dates.get(1));
+            }
+            else{
+                tvSearchCriteria.setText(dates.get(0));
+            }
+            if (result.isEmpty()){
                 tvEarthquakeNo.setText("No Earthquakes Found");
             }
             else {
-                getEarthquakeSummary(filteredList);
-                tvEarthquakeNo.setText("Number of Earthquakes Found: " + filteredList.size());
+                getEarthquakeSummary();
+                tvEarthquakeNo.setText("Number of Earthquakes Found: " + result.size());
             }
         }
     }
 
-    private void getEarthquakeSummary(ArrayList<Earthquake> earthquakeList) {
+    private void getEarthquakeSummary() {
         String north = northEq.getLocation() + "\n" + northEq.getGeoLat() + "," + northEq.getGeoLong();
         tvNorth.setText(north);
         String south = southEq.getLocation() + "\n" + southEq.getGeoLat() + "," + southEq.getGeoLong();
